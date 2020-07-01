46caaf05f103fe5b46bd7f1f7203df836e0e3284
M krishna chaitanya <krishnachaitanyajnv22@gmail.com>
2020-07-01 21:51:31 +0530

    added ads

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,17 @@ function App() {
         <p className="stateHeader">StateWise Analysis</p>
         <State/>
       </section>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      {/* <!-- covid19-counter ad --> */}
+      <ins className="adsbygoogle"
+          style={{display:'block'}}
+          data-ad-client="ca-pub-7333060626743598"
+          data-ad-slot="3593859641"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+      <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
     </div>
   );
 }
